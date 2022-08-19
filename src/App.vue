ccb080eae05000c66398d6be175ea4e872e27858
Dani D <daniserra14@gmail.com>
2022-08-19 20:33:20 +0200

    modal + search

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,32 @@
 <script>
+import PickerModal from "./components/pickerModal.vue";
+
 export default {
   name: "App",
   data() {
     return {
       version: "12.12.1",
+
+      //champions
       rawChampionList: {},
       championScoreList: JSON.parse(
         localStorage.getItem("championScoreList") || "{}"
       ),
+
+      //tags
       availableTags: [],
       selectedTag: null,
+
+      // rating modal
+      selectedChampionId: null,
+
+      //search
+      searchValue: "",
     };
   },
   computed: {
     championList() {
       const championImgRoute = this.ddragonRoutes.championImg;
-
       let championList = Object.values(this.rawChampionList)
         .filter(
           (champion) =>
@@ -23,14 +34,30 @@ export default {
             champion.tags.find((tag) => tag == this.selectedTag)
         )
         .map((champion) => {
+          if (!this.championScoreList[champion.id]) {
+            this.championScoreList[champion.id] = {
+              top: 0,
+              jungle: 0,
+              mid: 0,
+              bottom: 0,
+              support: 0,
+            };
+          }
+
           return {
             id: champion.id,
             name: champion.name,
             image: championImgRoute + champion.image.full,
             tags: champion.tags,
-            score: this.championScoreList[champion.id] || 0,
+            score: this.championScoreList[champion.id],
           };
         });
+
+      if (this.searchValue != "") {
+        championList = championList.filter((champion) =>
+          champion.name.toLowerCase().includes(this.searchValue.toLowerCase())
+        );
+      }
 
       return championList;
     },
@@ -40,9 +67,7 @@ export default {
       console.log(this.ddragonRoutes.versions);
       const versionResponse = await fetch(this.ddragonRoutes.versions);
       const versions = await versionResponse.json();
-
       this.version = versions[0];
-
       // update ddragon routes with latest version
       Object.keys(this.ddragonRoutes).forEach((key) => {
         this.ddragonRoutes[key] = this.ddragonRoutes[key].replace(
@@ -51,24 +76,38 @@ export default {
         );
       });
     },
-
     async fetchChampionList() {
       const championsResponse = await fetch(this.ddragonRoutes.champions);
       const champions = (await championsResponse.json()).data;
-
       console.log("champions", champions);
-
       this.rawChampionList = champions;
     },
-
     composeCSSBgImg(image) {
       return `background-image: url('${image}')`;
+    },
+    updateChampionScore(champion, score) {
+      this.championScoreList[champion] = score;
+
+      localStorage.setItem(
+        "championScoreList",
+        JSON.stringify(this.championScoreList)
+      );
+    },
+    getChampionBorderColorClass(champion) {
+      let maxChampionScore = Math.max(...Object.values(champion.score));
+      let color = this.scoreColors[maxChampionScore - 1];
+
+      //border-red-600
+      //border-orange-600
+      //border-yellow-600
+      //border-lime-600
+      //border-green-600
+      return color ? `border-${color}-600` : "";
     },
   },
   async created() {
     await this.fetchLastVersion();
     await this.fetchChampionList();
-
     // Create filter tags list
     Object.values(this.rawChampionList).forEach((champion) => {
       Object.values(champion.tags).forEach((tag) => {
@@ -78,6 +117,7 @@ export default {
       });
     });
   },
+  components: { PickerModal },
 };
 </script>
 
@@ -85,6 +125,35 @@ export default {
   <div class="container mx-auto">
     <h1>LolPick</h1>
     <p>Version: {{ version }}</p>
+    <form class="my-3 flex items-center">
+      <label for="simple-search" class="sr-only">Search</label>
+      <div class="relative w-full">
+        <div
+          class="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3"
+        >
+          <svg
+            aria-hidden="true"
+            class="h-5 w-5 text-gray-500 dark:text-gray-400"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </div>
+        <input
+          type="text"
+          id="simple-search"
+          class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 pl-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500"
+          placeholder="Search"
+          v-model="searchValue"
+        />
+      </div>
+    </form>
     <div id="tags" class="flex justify-center gap-3">
       <button
         v-for="tag in availableTags"
@@ -100,15 +169,29 @@ export default {
       <div
         v-for="champion in championList"
         :key="champion.id"
-        class="champion h-[120px] w-[120px] cursor-pointer bg-cover transition-shadow duration-300 hover:shadow-lg"
+        class="champion h-[120px] w-[120px] cursor-pointer bg-cover transition-all duration-300 hover:scale-110 hover:shadow-xl"
         :style="composeCSSBgImg(champion.image)"
+        @click="selectedChampionId = champion.id"
       >
-        <div class="flex h-full items-end justify-center border-4">
+        <div
+          class="flex h-full items-end justify-center border-4"
+          :class="getChampionBorderColorClass(champion)"
+        >
           <span class="w-full bg-black/60 text-center text-white">{{
             champion.name
           }}</span>
         </div>
       </div>
     </div>
+    <PickerModal
+      v-if="selectedChampionId"
+      :championScore="championScoreList[selectedChampionId]"
+      :championId="selectedChampionId"
+      :championName="rawChampionList[selectedChampionId].name"
+      @remove-selected-champion="selectedChampionId = null"
+      @update-selected-champion-score="
+        (score) => updateChampionScore(selectedChampionId, score)
+      "
+    />
   </div>
 </template>
---
+++ src/components/pickerModal.vue
@@ -0,0 +1,114 @@
+<script>
+export default {
+  data() {
+    return {
+      //TODO: Make it a computed property
+      newChampionScore: this.championScore,
+      // scoreColors:
+      // bg-red-200 bg-red-600
+      // bg-orange-200 bg-orange-600
+      // bg-yellow-200 bg-yellow-600
+      // bg-lime-200 bg-lime-600
+      // bg-green-200 bg-green-600
+    };
+  },
+  props: {
+    championScore: Object,
+    championId: String,
+    championName: String,
+  },
+  methods: {
+    updateCurrentChampionScore(position, positionScore) {
+      this.newChampionScore[position] = positionScore;
+
+      this.$emit("updateSelectedChampionScore", this.newChampionScore);
+    },
+  },
+};
+</script>
+<style lang="scss">
+.modal-wrapper {
+  position: fixed;
+
+  .modal {
+    width: 600px;
+    max-width: 90%;
+    height: 500px;
+    background: #fff;
+    color: #000;
+  }
+}
+</style>
+<template>
+  <Transition>
+    <!-- <div class="modal-wrapper">
+      <div class="modal">{{ championScore }}</div>
+    </div> -->
+    <div
+      id="defaultModal"
+      tabindex="-1"
+      aria-hidden="true"
+      class="h-modal fixed top-0 right-0 left-0 z-50 w-full overflow-y-auto overflow-x-hidden md:inset-0 md:h-full"
+    >
+      <div class="relative m-auto h-full w-full max-w-2xl p-4 md:h-auto">
+        <!-- Modal content -->
+        <div class="relative rounded-lg bg-white shadow dark:bg-gray-700">
+          <!-- Modal header -->
+          <div
+            class="flex items-start justify-between rounded-t border-b p-4 dark:border-gray-600"
+          >
+            <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+              {{ championName }}
+            </h3>
+            <button
+              type="button"
+              class="ml-auto inline-flex items-center rounded-lg bg-transparent p-1.5 text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white"
+              data-modal-toggle="defaultModal"
+              @click="$emit('removeSelectedChampion')"
+            >
+              <svg
+                aria-hidden="true"
+                class="h-5 w-5"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+              <span class="sr-only">Close modal</span>
+            </button>
+          </div>
+          <!-- Modal body -->
+          <div class="space-y-6 p-6">
+            <div
+              class="score-position"
+              :key="position"
+              v-for="position in Object.keys(championScore)"
+            >
+              <p class="mb-2 text-white">{{ position }}</p>
+              <div class="grid grid-cols-5 gap-2">
+                <button
+                  v-for="(color, index) in scoreColors"
+                  :key="index"
+                  :class="
+                    (championScore[position] == index + 1
+                      ? `bg-${color}-600`
+                      : `bg-${color}-200`) + ` hover:bg-${color}-600`
+                  "
+                  class="rounded-lg border border-gray-200 px-5 py-2.5 text-sm font-bold text-black"
+                  @click="updateCurrentChampionScore(position, index + 1)"
+                >
+                  {{ index + 1 }}
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </Transition>
+</template>
