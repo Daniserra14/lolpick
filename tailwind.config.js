48a28828f1d9d43c0548ee717d8b69c67e42a7ef
Dani D <daniserra14@gmail.com>
2022-10-07 20:12:07 +0200

    tailwind conf

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: ["./public/**/*.html", "./src/**/*.{js,vue}"],
+  purge: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
