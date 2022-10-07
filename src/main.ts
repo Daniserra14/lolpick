ae7de03859d861dc0593451222e53725d61627b6
Dani D <daniserra14@gmail.com>
2022-10-07 21:58:16 +0200

    https ddragon route

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,8 @@ const app = createApp(App);
 app.config.globalProperties.ddragonRoutes = {
   versions: "https://ddragon.leagueoflegends.com/api/versions.json",
   champions:
-    "http://ddragon.leagueoflegends.com/cdn/{version}/data/en_US/champion.json",
-  championImg: "http://ddragon.leagueoflegends.com/cdn/{version}/img/champion/",
+    "https://ddragon.leagueoflegends.com/cdn/{version}/data/en_US/champion.json",
+  championImg: "https://ddragon.leagueoflegends.com/cdn/{version}/img/champion/",
 };
 
 app.config.globalProperties.scoreColors = [
