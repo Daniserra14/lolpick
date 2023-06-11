256f014f29ca897b136ffe7379f55015894f2ea5
Dani Domínguez <daniserra14@gmail.com>
2023-06-11 13:43:57 +0200

    style h1 rate

--- src/views/Rate.vue
+++ src/views/Rate.vue
@@ -36,7 +36,7 @@ export default {
 };
 </script>
 <template>
-  <h1>Unrated champions:</h1>
+  <h1 class="text-center text-xl my-8">Unrated champions</h1>
   <div class="my-4 grid grid-cols-3 gap-4 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-8">
     <Champion v-for="champion in unratedChampions" :key="champion.id" :champion="champion" />
   </div>
